22691e29b99f5f24461711cfdc641cd53a0e4263
Michael Crockett <michael.crockett@gmail.com>
2016-07-23 16:53:47 -0400

    modifying getTwitter request handler

--- app/containers/HomePage/sagas.js
+++ app/containers/HomePage/sagas.js
@@ -29,6 +29,25 @@ export function* getRepos() {
 }
 
 /**
+ * Twitter Data request/response handler
+ */
+export function* getTwitter() {
+  // Select username from store
+  const username = yield select(selectUsername());
+  const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
+
+  // Call our request helper (see 'utils/request')
+  const repos = yield call(request, requestURL);
+
+  if (!repos.err) {
+    yield put(reposLoaded(repos.data, username));
+  } else {
+    yield put(repoLoadingError(repos.err));
+  }
+}
+
+
+/**
  * Watches for LOAD_REPOS action and calls handler
  */
 export function* getReposWatcher() {
@@ -42,7 +61,7 @@ export function* getReposWatcher() {
  */
 export function* getTwitterWatcher() {
   while (yield take(LOAD_TWITTER)) {
-    yield call(getRepos);
+    yield call(getTwitter);
   }
 }
 
